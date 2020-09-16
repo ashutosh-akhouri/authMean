debb3b77b35890bfca8a986f6831594859ea608a
ashutosh_k_akhouri <ashutosh.akhouri@gmail.com>
2020-09-16 09:53:18 +0530

    JWT

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+var ejwt = require('express-jwt');
 
 // DON'T HARDCODE connection string here, read it from process.env
-const connectionString = "mongodb+srv://bz:bz@cluster0-li9qp.mongodb.net/meanauth?retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://bz:bz@cluster0-li9qp.mongodb.net/meanauth1?retryWrites=true&w=majority";
 const dbOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, auto_reconnect: true };
 mongoose.connect(connectionString, dbOptions);
 mongoose.connection.on('connected', function () {
@@ -13,8 +16,88 @@ mongoose.connection.on('error', function (err) {
 });
 
 const app = express();
-app.use(express.static(__dirname+'/dist/meanAuth'));
+app.use(express.static(__dirname + '/dist/meanAuth'));
 app.use(express.json());
+app.use(cors());
+
+const { OAuth2Client } = require('google-auth-library');
+const { stringify } = require('querystring');
+const client = new OAuth2Client('562036829081-rr70prvp4fe55vjb1m6iui5l4ep9ha7l.apps.googleusercontent.com');
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: '562036829081-rr70prvp4fe55vjb1m6iui5l4ep9ha7l.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+
+    return payload;
+}
+
+
+var userSchema = new mongoose.Schema({
+    email: String,
+    google: {
+        id: String,
+        photoUrl: String,
+        name: String
+    },
+    facebook: {
+        id: String,
+        photoUrl: String,
+        name: String
+    }
+})
+
+// instance methods
+userSchema.methods.genToken = function () {
+    var token = jwt.sign({
+        name: this.google.name,
+        email: this.email,
+        photoUrl: this.google.photoUrl,
+        role: 'Normal'
+    }, 'SECRET....SHHH', { expiresIn: '1d' });
+
+    return token;
+}
+
+var User = mongoose.model('User', userSchema);
+
+app.post('/api/login/google', async (req, res) => {
+    // console.log(req.body);
+    try {
+        let payload = await verify(req.body.token);
+        console.log(JSON.stringify(payload));
+
+        let user = await User.findOne({ email: payload.email });
+        if (!user) {
+            console.log("User not found");
+            var newUser = new User(
+                { email: payload.email, google: { id: payload.sub, name: payload.name, photoUrl: payload.picture } }
+            );
+            user = await newUser.save();
+            console.log("User saved in db");
+        }
+        var jwToken = user.genToken();
+        res.json({ msg: "Okay", token: jwToken });
+    } catch (e) {
+        console.log('Err while g verify ' + e);
+        res.status(401).json({ msg: "Noooo" });
+    }
+})
+
+// check for token and if token is valid, then allow
+app.use(ejwt({secret: 'SECRET....SHHH', algorithms:['HS256']}));
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send('invalid token...');
+    }
+});
+
+app.get('/api/profile', (req, res) => {
+    res.json({ profile: "You are able to access" });
+})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log("Server started...") });
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
 import { BehaviorSubject } from 'rxjs';
@@ -7,15 +8,22 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class AuthService {
 
+  static url = "http://localhost:3000/";
+
   currentUser: User = new User();
   userStatus = new BehaviorSubject<User>(this.currentUser);
 
-  constructor(private socialAuth: SocialAuthService) { }
+  constructor(private socialAuth: SocialAuthService, private http: HttpClient) { }
 
   async doLogin() {
     // Login success
     try {
       let socialUser = await this.socialAuth.signIn(GoogleLoginProvider.PROVIDER_ID);
+      
+      let res = await this.http.post(AuthService.url+'api/login/google', {token: socialUser.idToken}).toPromise();
+      console.log(res);
+
+      
       this.currentUser.setUser(true, socialUser);
       console.log(JSON.stringify(socialUser));
       this.userStatus.next(this.currentUser);
